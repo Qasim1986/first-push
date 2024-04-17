ee9aaa14e7bbd160fd7f0255d1bd59538da6e208
Qasim1986 <163725332+Qasim1986@users.noreply.github.com>
2024-04-17 22:35:51 +0500

    Add files via upload

---
+++ hello.ts
@@ -0,0 +1,166 @@
+// // Question 1.
+// let greetings = "hello world!";
+// console.log(greetings);
+
+// // // Question 2
+// let num1 = 10;
+// let num2 = 20;
+// let sum = num1 + num2;
+// console.log(sum);
+// let difference = num2 - num1;
+// console.log(difference);
+// let multiplication = num1 * num2;
+// console.log(multiplication);
+// let division = num2 / num1;
+// console.log(division);
+
+// // // Question 3
+// let a = 1;
+// let b = 2;
+// [a, b] = [b, a];
+// console.log(a);
+// console.log(b);
+
+// Question 4.
+// let myGoal: string = "developer";
+// console.log(myGoal);
+// // let myGoal1: number= "developer"  changing type is reflecting error.
+
+// Questlion 5.
+// let number1 = 20;
+// let number2 = 10;
+// let modulus = number1 % number2;
+// console.log(modulus);
+
+// Question 6
+// let X = 10;
+// X = 10 + 1;
+// console.log(X);
+
+// Questlion 7.
+//  AND pinfiple.
+// console.log(true && true);
+// console.log(true && false);
+// console.log(false && true);
+// console.log(false && false);
+// Practice of AND.
+// let a = 20;
+// let b = 30;
+// console.log(a === 20 && b === 30);
+// console.log(a === 20 && b === 10);
+// console.log(a === 10 && b === 30);
+// console.log(a === 10 && b === 40);
+
+// // OR principle
+// // console.log(true || true);
+// // console.log(true || false);
+// // console.log(false || true);
+// // console.log(false || false);
+// // practice of OR
+// console.log(a === 20 || b === 30);
+// console.log(a === 20 || b === 40);
+// console.log(a === 10 || b === 30);
+// console.log(a === 10 || b === 40);
+
+// // NOT principle
+// // console.log(!true===false);
+// // NOT practice
+// console.log(a !== 20);
+// console.log(b !== 30);
+
+// Question 8.
+// let a = 5;
+// a += 2;
+// console.log("using +=", a);
+// a -= 4;
+// console.log("using -=", a);
+// a *= 6;
+// console.log("using *=", a);
+// a /= 10;
+// console.log("using /=", a);
+
+// Question 09 even or odd.
+// let num3 = 35;
+// if (num3 % 2 === 0) {
+//   console.log("number is even ");
+// } else {
+//   console.log("number is odd");
+// }
+// let num3a = 38;
+// if (num3a % 2 === 0) {
+//   console.log("number is even ");
+// } else {
+//   console.log("number is odd");
+// }
+
+// Question 10. voting eligibility
+// let voterAge = 19;
+// if (voterAge >= 18) {
+//   console.log("he is eligibl for voting");
+// } else {
+//   console.log("he is not eligible for voting");
+// }
+// let voterAge1 = 17;
+// if (voterAge1 >= 18) {
+//   console.log("he is eligibl for voting");
+// } else {
+//   console.log("he is not eligible for voting");
+// }
+
+// Question 11. Grading system.
+
+// let num4 = 67;
+// if (num4 >= 90 && num4 <= 100) {
+//   console.log("your grade is A++");
+// } else if (num4 >= 80 && num4 <= 89) {
+//   console.log("your grade is A+");
+// } else if (num4 >= 70 && num4 <= 79) {
+//   console.log("your grade is A");
+// } else if (num4 >= 60 && num4 <= 79) {
+//   console.log("your grade is B");
+// } else if (num4 >= 50 && num4 <= 59) {
+//   console.log("your grade is C");
+// } else {
+//   console.log("you are fail");
+// }
+// Question 12. Maximum of three values.
+// let p = 1;
+// let q = 2;
+// let r = 3;
+// if (p > q && p > r) {
+//   console.log("Maximum of three is p =" + p);
+// } else if (q > p && q > r) {
+//   console.log("Maximum of three is i q =" + q);
+// } else {
+//   console.log("Maximum of three is r =" + r);
+// }
+// Question 13.
+// let year = 2024;
+// if (year % 4 === 0) {
+//   console.log("this is leap year");
+// } else {
+//   console.log("this is not leap year");
+// }
+// Question 14 farhenheit to celcius.
+// let farhenHeit = 98.8;
+// let Celsius = ((farhenHeit - 32) * 5) / 9;
+// console.log("temperature in celsius is " + Celsius);
+// Question 15. number checking
+// let numBer = -4;
+// if (numBer < 0) {
+//   console.log("the number is negative");
+// } else if (numBer > 0) {
+//   console.log("the number is positive");
+// } else {
+//   console.log("the number is zero");
+// }
+
+// Question 16 Table.
+// let m = 1;
+// while (m <= 10) {
+//   console.log(`3*${m} = ${m * 3}`);
+//   m++;}
+// with for loop
+// for (let n = 1; n <= 10; n++) {
+//   console.log(`n*${3}=${n * 3}`);
+// }
